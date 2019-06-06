d99269e70db1bfcf86942c005514acdba7ba2741
Noam_Levy <noamlv@mta.ac.il>
2019-06-06 17:18:57 +0300

    exit or continue form

---
+++ View/ExitOrContinueForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class ExitOrContinueForm : Form
+    {
+        
+        public ExitOrContinueForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitOrContinueForm_Load(object sender, EventArgs e)
+        {
+            string message = "some message";
+            MessageBox.Show(message);
+        }
+    }
+}
